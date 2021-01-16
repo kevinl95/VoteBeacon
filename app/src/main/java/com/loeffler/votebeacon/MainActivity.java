59dda7c71ed58b7cc822889aa43811bb55acba49
kevinl95 <kevin.loeffler@colorado.edu>
2021-01-16 15:38:11 -0700

    Fixing syntax error.

--- app/src/main/java/com/loeffler/votebeacon/MainActivity.java
+++ app/src/main/java/com/loeffler/votebeacon/MainActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import com.uriio.beacons.Beacons;
 import com.uriio.beacons.model.Beacon;
@@ -19,15 +19,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Beacons.initialize(this);
-        final ImageButton beaconbutton = (ImageButton)findViewById(R.id.beaconbutton);
+        final ImageView beaconbutton = (ImageView) findViewById(R.id.beaconlayoutbutton);
         beaconbutton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 if (beaconOn == false) {
                     beaconbutton.setImageResource(R.drawable.beaconon);  // Display that the beacon is on
                     urlBeacon.start();  // Start broadcasting
+                    beaconOn = true;
                 } else {
                     beaconbutton.setImageResource(R.drawable.beaconoff);  // Display that the beacon is off
                     urlBeacon.stop();  // Stop broadcasting
+                    beaconOn = false;
                 }
             }
         });
